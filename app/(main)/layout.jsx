bcc7c326a9a859db15352e02ae7926c2bb1b3937
fuzail khan <ifuzail1234@gmail.com>
2023-11-27 16:43:47 +0530

    auth changes

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -6,16 +6,24 @@ import { RightSidebar } from "@/app/components/navigation/RightSidebar";
 import { Topbar } from "@/app/components/navigation/Topbar";
 import { useUserContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { Loading } from "../components/shared/Loading";
 
 
 const MainLayout = ({ children }) => {
 
   const router = useRouter();
-  const { user } = useUserContext();
+  const { isAuthenticated } = useUserContext();
 
-  if (!user) {
-    router.push("/login");
-    return null;
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/');
+    }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated) {
+    return (
+    <Loading />
+    );
   }
 
   return (
