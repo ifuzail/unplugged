a279f6c9cc6e4083f33c38d6a2a68fcef2e50e0f
fuzail khan <ifuzail1234@gmail.com>
2023-11-27 17:12:13 +0530

    auth changes

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -4,23 +4,34 @@ import { BottomBar } from "@/app/components/navigation/BottomBar";
 import { LeftSidebar } from "@/app/components/navigation/LeftSidebar";
 import { RightSidebar } from "@/app/components/navigation/RightSidebar";
 import { Topbar } from "@/app/components/navigation/Topbar";
+import { useUserContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 
 const MainLayout = ({ children }) => {
 
-  return (
-        <div className="flex flex-row gap-1">
-          <LeftSidebar />
-          <div className="md:w-[50%] w-full flex flex-col justify-between">
-            <div className="flex flex-col">
-              <Topbar />
-              <div className="md:mt-0 mt-16 md:mb-0 mb-12">{children}</div>
-              <BottomBar />
+  const router = useRouter();
+  const {isAuthenticated} = useUserContext();
+
+  if(isAuthenticated) {
+      router.push('/login')
+      return null
+  } else {
+      return (
+            <div className="flex flex-row gap-1">
+              <LeftSidebar />
+              <div className="md:w-[50%] w-full flex flex-col justify-between">
+                <div className="flex flex-col">
+                  <Topbar />
+                  <div className="md:mt-0 mt-16 md:mb-0 mb-12">{children}</div>
+                  <BottomBar />
+                </div>
+              </div>
+              <RightSidebar />
             </div>
-          </div>
-          <RightSidebar />
-        </div>
-  );
+      );
+  }
+  
 };
 
 export default MainLayout;
