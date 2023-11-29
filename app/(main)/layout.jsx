42c01d272d01bbec65974e90877a697be811e2ab
fuzail khan <ifuzail1234@gmail.com>
2023-11-29 18:38:32 +0530

    auth changes

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -4,8 +4,28 @@ import { BottomBar } from "@/app/components/navigation/BottomBar";
 import { LeftSidebar } from "@/app/components/navigation/LeftSidebar";
 import { RightSidebar } from "@/app/components/navigation/RightSidebar";
 import { Topbar } from "@/app/components/navigation/Topbar";
+import { useUserContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const MainLayout = ({ children }) => {
+  const router = useRouter();
+  const { checkAuthUser } = useUserContext();
+
+  
+useEffect(() => {
+  const checkAuth = async () => {
+    const isLoggedIn = await checkAuthUser();
+
+    if(!isLoggedIn) {
+      router.push('/login')
+    }
+  }
+
+  checkAuth();
+}, [])
+
+  
   return (
     <div className="flex flex-row gap-1">
       <LeftSidebar />
